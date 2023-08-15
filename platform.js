085c68c4b894fb57438e76c947ee563f53a765d1
liam-zel <lzelmanowski@gmail.com>
2023-08-15 10:20:46 +1000

    Powerup support + Powerup generation

--- platform.js
+++ platform.js
@@ -15,15 +15,23 @@ class Platform {
         this.attachedPowerup = undefined
     }
 
+    
     draw() {
         strokeWeight(3)
         fill(this.col)
         rect(this.x, this.y, this.w, this.h)
+
+        // draw powerup
+        if (this.attachedPowerup !== undefined) {
+            this.attachedPowerup.draw()
+        }
     }
 
-    // generic function to run every frame alongside draw
-    update() {}
 
+    update() {} // generic function to run every frame alongside draw
+
+
+    // runs when player collides with platform while falling
     onCollision(plr) {
         plr.jump()
     }
@@ -70,9 +78,12 @@ class movingPlatform extends Platform {
     update() {
         this.x += this.moveSpeed * this.direction
 
+        if (this.attachedPowerup !== undefined) this.attachedPowerup.x += this.moveSpeed * this.direction
+
         if (this.x + this.w > Scene.rightBorder || this.x < Scene.leftBorder) {
             this.direction *= -1
         }
+
     }
 }
 
@@ -82,6 +93,8 @@ class movingPlatform extends Platform {
  * Generates a platform with random type, x position and y position
  */
 function generatePlatform() {
+    const powerupChance = 10 // 1 in powerupChance
+
     // platforms can generate too far in advance, this prevents that
     if (activePlatforms.length > 0 && activePlatforms[activePlatforms.length-1].y < -500) return
 
@@ -90,19 +103,43 @@ function generatePlatform() {
         (x, y) => {return new Platform(x, y)},
         (x, y) => {return new BreakablePlatform(x, y)},
         (x, y) => {return new movingPlatform(x, y)},
-    ]
-
+    ]    
+    
     let index = floor(random(platformTypes.length))
-
+    
     let x = floor(random(Scene.leftBorder, Scene.rightBorder - 80))
-
+    
     // y position of previous platform, otherwise its the floors y position
     let previousY = Scene.floorHeight - 70
     if (activePlatforms.length > 0) previousY = activePlatforms[activePlatforms.length-1].y
-
+    
     let y = floor(random(previousY, previousY-130))
-
+    
     activePlatforms.push( platformTypes[index](x, y) )
+
+    // attach powerup (1 in 100 chance)
+    if (floor(random(powerupChance)) === 0 && !plr.hasPowerup) {
+        attachPowerup(activePlatforms[activePlatforms.length-1])
+    }
+}
+
+
+/**
+ * Creates and attaches a new powerup object to the given platform argument
+ * @param {Object} platform a given platform object to have a powerup attached to it
+ */
+function attachPowerup(platform) {
+    // array of functions
+    const powerups = [
+        (p) => {return new Jetpack(p)},
+    ] 
+
+    let index = floor(random(powerups.length))
+
+    let powerup = powerups[index](platform)
+
+    platform.attachedPowerup = powerup
+    activePowerups.push(powerup)
 }
 
 
@@ -113,6 +150,7 @@ function deletePlatforms() {
     activePlatforms.forEach(platform => {
 
         if (platform.y > Scene.height) {
+            if (platform.attachedPowerup !== undefined) activePowerups.splice(activePowerups.indexOf(platform.attachedPowerup), 1)
             activePlatforms.splice(activePlatforms.indexOf(platform), 1)
         }
 
