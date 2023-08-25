611ab97f5e5d86b8910f82e6ea7d733b16fed387
liam-zel <lzelmanowski@gmail.com>
2023-08-25 13:23:50 +1000

    Updated Settings

--- settings.js
+++ settings.js
@@ -1,4 +1,30 @@
 // -------------------- SETTINGS OBJECT --------------------
+/**
+ * Global settings object which stores player preferences
+ */
 const Settings = {
-    inputMode: 'mouse' // modes: 'mouse', 'keys'
+    inputModes: {
+        mouse: 1,
+        keys: 2,
+    },
+    currentInputMode: undefined,
+
+    globalVolume: 0.2,
+
+    supportedLanguages: {
+        english: 1,
+        spanish: 2,
+        polish: 3,
+    },
+    language: undefined,
+}
+
+
+// -------------------- FUNCTIONS --------------------
+/**
+ * Set default settings values
+ */
+function initSettings() {
+    Settings.currentInputMode = Settings.inputModes.mouse
+    Settings.language = Settings.supportedLanguages.english
 }
