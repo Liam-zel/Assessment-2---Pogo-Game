44efd560c6de4b84a5184aacf31026507cc3f419
liam-zel <lzelmanowski@gmail.com>
2023-08-25 13:22:54 +1000

    New states, the state of the program is a mess

--- gameStateManager.js
+++ gameStateManager.js
@@ -4,40 +4,108 @@
  */
 const GameState = {
     states: {
-        start: {
+        /**
+         * Generate obstacles and re-initialise game
+         */
+        generate: {
+            initState: () => { 
+                initaliseGame()
+                createObstacles() 
+            },
+            function: () => { 
+                changeGameState(GameState.states.start) 
+            }
+        },
+
+        restart: {
             initState: () => {
                 initaliseGame()
+                createObstacles()
+            },
+            function: () => {
+                changeGameState(GameState.states.game)
+            }
+        },
 
-                const playButton = new StateButton(Scene.leftBorder + Scene.width / 2, Scene.floorHeight - 100, 180, 180) 
+        /**
+         * Start screen, first thing presented to user
+         */
+        start: {
+            initState: () => {
+
+                Game.ambientSound = Sounds.birdAmbience
+                playSound(Game.ambientSound, true)
+
+                // --- buttons ---
+                const playButton = new StateButton(Scene.leftBorder + Scene.width / 2 + 50, Scene.floorHeight - 100, 180, 180) 
                 playButton.setSprite(Sprites.playButton)
                 playButton.setState(GameState.states.game)
-                activeButtons.push(playButton)
 
-                createObstacles()
+                const settingsButton = new StateButton(Scene.leftBorder + Scene.width / 2 - 150, Scene.floorHeight - 100, 110, 110)
+                settingsButton.setSprite(Sprites.settingsButton)
+                settingsButton.setState(GameState.states.settings)
+
+                activeInteractions = [playButton, settingsButton]
+
             },
             function: startMenu
         },
+
+        /**
+         * Gameplay
+         */
         game: {
             initState: () => {
-                activeButtons = []
+                activeInteractions = []
                 plr = new Player(Scene.width/2, Scene.floorHeight - 20)
             },
             function: runGame
         },
+        
+        /**
+         * Settings screen
+         */
         settings: {
             initState: () => {
+                const backButton = new StateButton(Scene.leftBorder + 100, Scene.floorHeight - 100, 110, 110)
+                backButton.setSprite(Sprites.backButton)
+                backButton.setState(GameState.states.start)
 
+                const volumeSlider = new Slider(Scene.leftBorder + Scene.width / 2, 210,
+                                                200, 35)
+
+                activeInteractions = [backButton, volumeSlider]
             },
-            function: () => {
-
-            }
+            function: settings
         },
+
+        /**
+         * Runs when player dies
+         */
         death: {
-            initState: () => {
-                
-            },
+            initState: () => {},
             function: playerDeath
         },
+
+        /**
+         * Game over screen
+         */
+        gameOver: {
+            initState: () => {
+                // --- buttons ---
+                const playButton = new StateButton(Scene.leftBorder + Scene.width / 2 + 50, Scene.floorHeight - 100, 180, 180) 
+                playButton.setSprite(Sprites.playButton)
+                playButton.setState(GameState.states.restart)
+
+                const backButton = new StateButton(Scene.leftBorder + 100, Scene.floorHeight - 100, 110, 110)
+                backButton.setSprite(Sprites.backButton)
+                backButton.setState(GameState.states.death)
+
+                activeInteractions = [playButton, backButton]
+            },
+            function: gameOver
+        },
+
     },
 
     currentState: undefined
@@ -63,20 +131,30 @@ function changeGameState(state) {
 function startMenu() {
     // --- background ---
     sprite(Sprites.background, Scene.leftBorder, 0, Scene.width, Scene.height)
-
+    
+    
     // --- platforms, powerups, enemies ---
     visiblePlatforms.forEach(platform => {
         platform.draw()
         platform.update()
     })
-
+    
     visibleEnemies.forEach(enemy => {
         enemy.draw()
         enemy.update()
     })
+    
+    
+    // --- title ---
+    imageMode(CENTER)
+    sprite(Sprites.title, 
+           Scene.leftBorder + Scene.width/2, 100,
+           400, 250)
+    imageMode(CORNER)
 
-    activeButtons.forEach(button => {
-        button.draw()
+    // --- button elements
+    activeInteractions.forEach(interaction => {
+        interaction.draw()
     })
 
     drawWindowBorder()
@@ -130,6 +208,100 @@ function runGame() {
 }
 
 
+function settings() {
+    // --- background ---
+    sprite(Sprites.background, Scene.leftBorder, 0, Scene.width, Scene.height)
+    
+    
+    // --- platforms, powerups, enemies ---
+    visiblePlatforms.forEach(platform => {
+        platform.draw()
+        platform.update()
+    })
+    
+    visibleEnemies.forEach(enemy => {
+        enemy.draw()
+        enemy.update()
+    })
+
+    // shade box to tint background
+    fill(0, 100)
+    rect(Scene.leftBorder, 0, Scene.width, Scene.height)
+
+
+    textSize(60)
+    textAlign(CENTER, CENTER);
+    strokeWeight(Game.textStyling.outlineSize)
+    fill(Game.textStyling.colour)
+
+    text("Settings", Scene.leftBorder + Scene.width / 2, 100)
+
+    textSize(45)
+    text("Volume: ", Scene.leftBorder + 100, 200)
+
+    Settings.globalVolume = activeInteractions[1].value
+    Howler.volume(Settings.globalVolume)
+
+    // --- button elements
+    activeInteractions.forEach(interaction => {
+        interaction.draw()
+        interaction.checkHeld()
+    })
+
+    drawWindowBorder()
+}  
+
+
+/**
+ * When player dies
+ */
 function playerDeath() {
+    initaliseGame()
+    createObstacles()
+    
     changeGameState(GameState.states.start)
+}
+
+
+/**
+ * Game over ui
+ */
+function gameOver() {
+    // --- background ---
+    sprite(Sprites.background, Scene.leftBorder, 0, Scene.width, Scene.height)
+        
+
+
+    // --- platforms, powerups, enemies ---
+    visiblePlatforms.forEach(platform => {
+        platform.draw()
+        platform.update()
+    })
+    
+    visibleEnemies.forEach(enemy => {
+        enemy.draw()
+        enemy.update()
+    })
+
+    // shade box to tint background
+    fill(0, 100)
+    rect(Scene.leftBorder, 0, Scene.width, Scene.height)
+
+    let finalScore = Game.score
+    textSize(60)
+    fill(Game.textStyling.colour)
+    textAlign(CENTER, CENTER)
+    text("Game Over", Scene.leftBorder + Scene.width / 2, 100)
+
+    textSize(45)
+    text(`Final Score: ${finalScore}m`, Scene.leftBorder + Scene.width / 2, 200)
+
+    // --- button elements
+    activeInteractions.forEach(interaction => {
+        interaction.draw()
+        interaction.checkHeld()
+    })
+
+    drawWindowBorder()
+    // changeGameState(GameState.states.death)
 }
